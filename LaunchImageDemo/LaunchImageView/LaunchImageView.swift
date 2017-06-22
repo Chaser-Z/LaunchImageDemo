30925af0ea7fd241b7e8e914a5bb878c9b648fa5
Chaser-Z <18330570306@163.com>
2017-06-22 20:23:58 +0800

    first commit

---
+++ LaunchImageDemo/LaunchImageView/LaunchImageView.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchImageView.swift
+//  LaunchImageDemo
+//
+//  Created by yeeaoo on 16/6/24.
+//  Copyright © 2016年 枫韵海. All rights reserved.
+//
+
+import UIKit
+
+class LaunchImageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ LaunchImageDemo/TestViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TestViewController.swift
+//  LaunchImageDemo
+//
+//  Created by yeeaoo on 16/6/27.
+//  Copyright © 2016年 枫韵海. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
